bac89172ec342a1ab185c6bd9cf9a8b6dc459845
ajloveslily14 <ajloveslily@gmail.com>
2026-02-06 13:55:01 -0600

    Some cleaning up and comments
    comments

--- twinkle.cpp
+++ twinkle.cpp
@@ -9,10 +9,8 @@
 
 
 typedef RZRESULT(*INIT)();
-typedef RZRESULT(*CREATEEFFECT)(RZDEVICEID DeviceID, ChromaSDK::EFFECT_TYPE Effect, PRZPARAM pParam, RZEFFECTID *pEffectId);
 typedef RZRESULT(*CREATEKEYBOARDEFFECT)(ChromaSDK::Keyboard::EFFECT_TYPE Effect, PRZPARAM pParam, RZEFFECTID *pEffectId);
 INIT Init = nullptr;
-CREATEEFFECT CreateEffect = nullptr;
 CREATEKEYBOARDEFFECT CreateKeyboardEffect = nullptr;
 
 
@@ -23,20 +21,20 @@ ChromaClass::ChromaClass():m_ChromaSDKModule(nullptr) {
 
 BOOL ChromaClass::Initialize() {
 	if (m_ChromaSDKModule == nullptr) {
-		m_ChromaSDKModule = ::LoadLibrary(CHROMASDKDLL);
+		m_ChromaSDKModule = ::LoadLibrary(CHROMASDKDLL); // Load the razer chroma SDK DLL
 		if (m_ChromaSDKModule == nullptr) {
 			return false;
 		}
 	}
 	if (Init == nullptr) {
 		auto Res = RZRESULT_INVALID;
-		Init = (INIT)::GetProcAddress(m_ChromaSDKModule,"Init");
+		Init = (INIT)::GetProcAddress(m_ChromaSDKModule,"Init"); // The documentation says this should be InitSDK but that'll crash if you try and set effects with it
 		if (Init) {
 			Res = Init();
 			if (Res == RZRESULT_SUCCESS){
-			CreateEffect = (CREATEEFFECT)::GetProcAddress(m_ChromaSDKModule, "CreateEffect");
+
 			CreateKeyboardEffect = (CREATEKEYBOARDEFFECT)::GetProcAddress(m_ChromaSDKModule, "CreateKeyboardEffect");
-			if (CreateEffect && CreateKeyboardEffect) {
+			if (CreateKeyboardEffect) {
 				return true;
 			} else {
 				return false;
@@ -58,11 +56,9 @@ struct star {
 std::vector<star> stars;
 
 
-
-
 int hueIndex = 0;
-void ChromaClass::DoColors() {
-	ChromaSDK::Keyboard::CUSTOM_EFFECT_TYPE kb = {};
+void ChromaClass::DoColors() { // This is our main effect loop
+	ChromaSDK::Keyboard::CUSTOM_EFFECT_TYPE kb = {}; // This initializes our custom keyboard effect which is a 2d maxtix based effect
 
 	hsv colorHSV = {};
 	colorHSV.h = hueIndex;
@@ -72,13 +68,13 @@ void ChromaClass::DoColors() {
 	COLORREF color = RGB(colorRGB.r*255,colorRGB.g*255,colorRGB.b*255);
 	for (size_t row = 0; row < ChromaSDK::Keyboard::MAX_ROW; row++) {
 		for (size_t col = 0; col < ChromaSDK::Keyboard::MAX_COLUMN; col++) {
-			kb.Color[row][col] = color;
+			kb.Color[row][col] = color; // flood the whole keyboard with color
 		}
 	}
 	
 	int randX = rand() % ChromaSDK::Keyboard::MAX_ROW;
 	int randY = rand() % ChromaSDK::Keyboard::MAX_COLUMN;
-	star newStar;
+	star newStar; // Intialize a new "star" to be rendered
 
 	newStar.row = randX;
 	newStar.col = randY;
@@ -86,18 +82,18 @@ void ChromaClass::DoColors() {
 	newStar.life = 1.0f;
 	stars.push_back(newStar);
 
-	for (auto si = stars.begin(); si != stars.end();) {
-		COLORREF dimmedColor = adjustBrightness(si->color,si->life);
-		kb.Color[si->row][si->col] = dimmedColor;
-		si->life -= 0.05f;
+	for (auto si = stars.begin(); si != stars.end();) { // Process "stars"
+		COLORREF dimmedColor = adjustBrightness(si->color,si->life); // Get our "dimmed" star color value
+		kb.Color[si->row][si->col] = dimmedColor; // Actually apply the star color
+		si->life -= 0.05f; // Decrement our star life 
 		if (si->life <= 0.0f){
-			si = stars.erase(si);
+			si = stars.erase(si); // Remove our star if it's dead
 		} else {
 			++si;
 		}
 	}
 
-	CreateKeyboardEffect(ChromaSDK::Keyboard::CHROMA_CUSTOM,&kb,nullptr);
+	CreateKeyboardEffect(ChromaSDK::Keyboard::CHROMA_CUSTOM,&kb,nullptr); // This creates and applies the keyboard effect we defined
 	hueIndex++;
 	hueIndex %= 361;
 
